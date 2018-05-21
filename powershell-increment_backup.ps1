62cc9a75d36f29fd64e02aced1d199cfcf4689a8
rptucker <36172640+rptucker@users.noreply.github.com>
2018-05-20 20:38:35 -0400

    Update powershell-increment_backup.ps1
    Corrected the time to check

--- powershell-increment_backup.ps1
+++ powershell-increment_backup.ps1
@@ -1,3 +1,3 @@
 $location = @("C:\Users\Administrator\Desktop\Increment\Windows","C:\Users\Administrator\Desktop\Increment\Windows2")
 $Destination = "C:\Users\Administrator\Desktop\Increment\Backup"
-Get-Item -LiteralPath $location -Force | Where{$_.LastWriteTime -ge (Get-Date).AddHours(-3)} | %{$_.FullName} {Copy-Item -Force -Recurse $_ -Destination ($Destination + "\" + $_.Name) -Container}
+Get-Item -LiteralPath $location -Force | Where{$_.LastWriteTime -ge (Get-Date).AddHours(-24)} | %{$_.FullName} {Copy-Item -Force -Recurse $_ -Destination ($Destination + "\" + $_.Name) -Container}
